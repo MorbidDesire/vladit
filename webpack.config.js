56129f10904c41e0fb20237b0b041167af062840
MorbidDesire <Ra1jinThunderkeg@yandex.ru>
2023-11-23 14:33:41 +0300

    installing

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const miniCss = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    open: true,
+    host: 'localhost',
+    port: 8080,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [ miniCss.loader, 'css-loader', 'sass-loader'],
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+    new miniCss({
+      filename: 'style.css',
+    })
+  ],
+};
